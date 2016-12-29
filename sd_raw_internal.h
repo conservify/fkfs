3d303f9b2816645c93f4bd7753323ac7b7663491
Jacob Lewallen <jlewallen@gmail.com>
2016-12-29 17:42:50 -0500

    Change commenting style.

--- sd_raw_internal.h
+++ sd_raw_internal.h
@@ -1,54 +1,54 @@
 #ifndef SD_RAW_INTERNAL_H_INCLUDED
 #define SD_RAW_INTERNAL_H_INCLUDED
 
-/** GO_IDLE_STATE - init card in spi mode if CS low */
+// GO_IDLE_STATE - init card in spi mode if CS low
 uint8_t const CMD0 = 0X00;
-/** SEND_IF_COND - verify SD Memory Card interface operating condition.*/
+// SEND_IF_COND - verify SD Memory Card interface operating condition.
 uint8_t const CMD8 = 0X08;
-/** SEND_CSD - read the Card Specific Data (CSD register) */
+// SEND_CSD - read the Card Specific Data (CSD register)
 uint8_t const CMD9 = 0X09;
-/** SEND_CID - read the card identification information (CID register) */
+// SEND_CID - read the card identification information (CID register)
 uint8_t const CMD10 = 0X0A;
-/** SEND_STATUS - read the card status register */
+// SEND_STATUS - read the card status register
 uint8_t const CMD13 = 0X0D;
-/** READ_BLOCK - read a single data block from the card */
+// READ_BLOCK - read a single data block from the card
 uint8_t const CMD17 = 0X11;
-/** WRITE_BLOCK - write a single data block to the card */
+// WRITE_BLOCK - write a single data block to the card
 uint8_t const CMD24 = 0X18;
-/** WRITE_MULTIPLE_BLOCK - write blocks of data until a STOP_TRANSMISSION */
+// WRITE_MULTIPLE_BLOCK - write blocks of data until a STOP_TRANSMISSION
 uint8_t const CMD25 = 0X19;
-/** ERASE_WR_BLK_START - sets the address of the first block to be erased */
+// ERASE_WR_BLK_START - sets the address of the first block to be erased
 uint8_t const CMD32 = 0X20;
-/** ERASE_WR_BLK_END - sets the address of the last block of the continuous
-    range to be erased*/
+// ERASE_WR_BLK_END - sets the address of the last block of the continuous
+// range to be erased
 uint8_t const CMD33 = 0X21;
-/** ERASE - erase all previously selected blocks */
+// ERASE - erase all previously selected blocks
 uint8_t const CMD38 = 0X26;
-/** APP_CMD - escape for application specific command */
+// APP_CMD - escape for application specific command
 uint8_t const CMD55 = 0X37;
-/** READ_OCR - read the OCR register of a card */
+// READ_OCR - read the OCR register of a card
 uint8_t const CMD58 = 0X3A;
-/** SET_WR_BLK_ERASE_COUNT - Set the number of write blocks to be
-    pre-erased before writing */
+// SET_WR_BLK_ERASE_COUNT - Set the number of write blocks to be
+// pre-erased before writing
 uint8_t const ACMD23 = 0X17;
-/** SD_SEND_OP_COMD - Sends host capacity support information and
-    activates the card's initialization process */
+// SD_SEND_OP_COMD - Sends host capacity support information and
+// activates the card's initialization process
 uint8_t const ACMD41 = 0X29;
-/** status for card in the ready state */
+// status for card in the ready state
 uint8_t const R1_READY_STATE = 0X00;
-/** status for card in the idle state */
+// status for card in the idle state
 uint8_t const R1_IDLE_STATE = 0X01;
-/** status bit for illegal command */
+// status bit for illegal command
 uint8_t const R1_ILLEGAL_COMMAND = 0X04;
-/** start data token for read or write single block*/
+// start data token for read or write single block
 uint8_t const DATA_START_BLOCK = 0XFE;
-/** stop token for write multiple blocks*/
+// stop token for write multiple blocks
 uint8_t const STOP_TRAN_TOKEN = 0XFD;
-/** start data token for write multiple blocks*/
+// start data token for write multiple blocks
 uint8_t const WRITE_MULTIPLE_TOKEN = 0XFC;
-/** mask for data response tokens after a write block operation */
+// mask for data response tokens after a write block operation
 uint8_t const DATA_RES_MASK = 0X1F;
-/** write data accepted token */
+// write data accepted token
 uint8_t const DATA_RES_ACCEPTED = 0X05;
 
 typedef struct cid_t {
