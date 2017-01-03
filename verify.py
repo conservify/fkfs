62efb83b3cf31ed4c20b304cf7903726a642c204
Jacob Lewallen <jlewallen@gmail.com>
2017-01-03 15:15:23 -0500

    Simple python script that verifies a log for correctness.

---
+++ verify.py
@@ -0,0 +1,46 @@
+import re
+
+file = open("log.txt", "r")
+
+rows = [re.findall(r'allocated f#(\d+)\.(\d+).(\d+)\s+(\d+)\[(\d+)\s*->\s*(\d+)\]', line) for line in open('log.txt')]
+matches = [x[0] for x in rows if x != []]
+
+class Allocation:
+    def __init__(self, file, priority, version, start, end):
+        self.file = file
+        self.priority = priority
+        self.version = version
+        self.start = start
+        self.end = end
+        self.r = range(start, end)
+
+    def overlaps(self, other):
+        return set(self.r).intersection(other.r)
+
+    def __repr__(self):
+        return "[{:d} {:d} {:d} {:d}]".format(self.file, self.priority, self.version, self.start, self.end)
+
+class Block:
+    def __init__(self):
+        self.allocated = []
+
+
+    def allocate(self, allocation):
+        for existing in self.allocated:
+            if existing.overlaps(allocation):
+                print [existing, allocation]
+        self.allocated.append(allocation)
+
+class Memory:
+    BlockSize = 512
+
+    def __init__(self):
+        self.blocks = {}
+
+    def allocate(self, block, allocation):
+        block = self.blocks.setdefault(block, Block())
+        block.allocate(allocation)
+
+card = Memory()
+for match in matches:
+    card.allocate(int(match[3]), Allocation(int(match[0]), int(match[1]), int(match[2]), int(match[4]), int(match[5])))
