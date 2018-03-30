20503bd6b7995aa970a4aeaf840318b1722fba66
Jacob Lewallen <jlewallen@gmail.com>
2018-03-30 09:46:16 -0700

    Iterators that begin after they end are invalid.

--- fkfs.cpp
+++ fkfs.cpp
@@ -626,7 +626,7 @@ uint8_t fkfs_file_iterator_done(fkfs_t *fs, fkfs_file_iter_t *iter) {
 }
 
 uint8_t fkfs_file_iterator_valid(fkfs_t *fs, fkfs_file_iter_t *iter) {
-    return iter->token.block > 0;
+    return iter->token.block > 0 && iter->token.block < iter->token.lastBlock && iter->token.block <= fs->header.block;
 }
 
 uint8_t fkfs_file_iterator_move_end(fkfs_t *fs, fkfs_file_iter_t *iter) {
