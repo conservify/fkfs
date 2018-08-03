b6d3450144b38972bf5ed82f1c49ccb8bc20ed13
Jacob Lewallen <jlewallen@gmail.com>
2018-08-03 09:09:07 -0700

    Add Jenkinsfile.

---
+++ Jenkinsfile
@@ -0,0 +1,7 @@
+@Library('conservify') _
+
+timestamps {
+    node () {
+        conservifyBuild(name: 'fkfs', repository: 'https://github.com/Conservify/fkfs.git')
+    }
+}
---
+++ examples/simple/dependencies.cmake
@@ -0,0 +1,8 @@
+set(EXTERNAL_DEPENDENCIES)
+# Library
+set(Adafruit_ASFcore_PATH "/home/jlewallen/fieldkit/fkfs/gitdeps/Adafruit_ASFcore")
+set(Adafruit_ASFcore_INFO "Adafruit_ASFcore" "/home/jlewallen/fieldkit/fkfs/gitdeps/Adafruit_ASFcore")
+list(APPEND EXTERNAL_DEPENDENCIES "Adafruit_ASFcore")
+
+# EndLibrary
+
